5da2dba05588f8de53132042158caf19d15571cc
Dastar Ron <dastar.ron@gmail.com>
2019-09-23 13:12:58 +0300

    filters.py and psd.py should be deleted

--- psd.py
+++ psd.py
@@ -1,20 +1,61 @@
-from PIL import Image
-from methods import add_frame
+import wx
 
 
-# b = Image.open('2.JPG')
-# f = Image.open('1.png')
-# #
-# # b.show()
-#
-# im = Image.new(f.mode, f.size)
-#
-# w = 1200
-# h = 800
-# b = b.resize((w, h), Image.ANTIALIAS)
-# # b.paste(f, (0, 0), f)
-# im.paste(b, (0,0))
-# im.paste(f, (0,0), f)
-# im.show()
+APP_EXIT = 1
 
-add_frame('1.png', '2.JPG')
+
+class Example(wx.Frame):
+    def __init__(self, *args, **kwargs):
+        super(Example, self).__init__(*args, **kwargs)
+        self.init_ui()
+
+    def init_ui(self):
+        menu_bar = wx.MenuBar()
+        view_menu = wx.Menu()
+
+        self.shst = view_menu.Append(wx.ID_ANY, 'show statusbar', 'show statusbar',
+                                     kind=wx.ITEM_CHECK)
+        self.shtl = view_menu.Append(wx.ID_ANY, 'show toolbar', 'show toolbar',
+                                     kind=wx.ITEM_CHECK)
+
+        view_menu.Check(self.shst.GetId(), True)
+        view_menu.Check(self.shtl.GetId(), True)
+
+        self.Bind(wx.EVT_MENU, self.toggle_status_bar, self.shst)
+        self.Bind(wx.EVT_MENU, self.toggle_tool_bar, self.shtl)
+
+        menu_bar.Append(view_menu, '&View')
+        self.SetMenuBar(menu_bar)
+
+        self.toolbar = self.CreateToolBar()
+        self.toolbar.AddTool(1, 'Some Text', wx.Bitmap('icon.png'))
+        self.toolbar.Realize()
+
+        self.statusbar = self.CreateStatusBar()
+        self.statusbar.SetStatusText('Ready')
+
+        self.SetSize((400, 300))
+        self.SetTitle('Simple Menu')
+        self.Center()
+
+    def toggle_status_bar(self, e):
+        if self.shst.IsChecked():
+            self.statusbar.Show()
+        else:
+            self.statusbar.Hide()
+
+    def toggle_tool_bar(self, e):
+        if self.shtl.IsChecked():
+            self.toolbar.Show()
+        else:
+            self.toolbar.Hide()
+
+def main():
+    app = wx.App()
+    ex = Example(None)
+    ex.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
