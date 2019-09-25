8d5cd9361e05c4d4f02a3d6124380569d960bf63
Dastar Ron <dastar.ron@gmail.com>
2019-09-25 12:45:12 +0300

    printer

--- printer.py
+++ printer.py
@@ -2,20 +2,40 @@ import subprocess
 
 
 class Printer:
+    def __init__(self, printer: str, default=[]):
+        self.printer = printer
+        self.default = default
+
+    def run(self, file, options: list = []):
+        """Prints file"""
+        cmd = ['lp', '-d', self.printer]
+        cmd += self.default
+        cmd += options
+
+        cmd.append(file)
+        subprocess.run(cmd)
+
+    def cancel(self, job=None):
+        """Cancel the job"""""
+        cmd = ['lprm', '-P', self.printer]
+        if job is list:
+            cmd += job
+        elif job is not None:
+            cmd.append(job)
+
+        subprocess.run(cmd)
+
     @staticmethod
     def list_all_printers() -> list:
-        result = subprocess.run(['lpstat', '-p'], stdout=subprocess.PIPE)
-        result = result.stdout.decode('utf-8')
-        return [i.split()[0]
-                for i in result.split('printer')
-                if i]
+        """Returns result of shell-command lpstat -p"""
+        result = subprocess.run(['lpstat', '-e'], stdout=subprocess.PIPE)
+        return result.stdout.decode('utf-8').split()
 
     @staticmethod
-    def get_page_size():
+    def get_printer_options() -> dict:
+        """Returns result of shell-command lpoptions -l"""
         result = subprocess.run(['lpoptions', '-l'], stdout=subprocess.PIPE)
         result = result.stdout.decode('utf-8')
-        return result
-
-
-if __name__ == '__main__':
-    print(Printer.get_page_size())
+        return dict((k.strip(), v.strip())
+                    for k, v in (item.split(':')
+                                 for item in result.split('\n') if item))
