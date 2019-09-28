f14f3e67c3e0caa3c4f0869b26b321f14c052632
Dastar Ron <dastar.ron@gmail.com>
2019-09-28 16:37:49 +0300

    working on photos

--- photo.py
+++ photo.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from os import path, remove
+import wx
 
 
 class Photo:
@@ -16,38 +17,58 @@ class Photo:
             print("Failed to open file " + self.path[1])
             return
 
-    def __call__(self):
+    def __call__(self, width=0):
         if self.framed is None:
-            return self.photo
+            img = self.photo
         else:
-            return self.framed
+            img = self.framed
+
+        if width != 0:
+            img = Photo.resize_photo(img.copy(), width)
+
+        return img
+
+    def undo(self):
+        self.photo = Image.open(path.join(*self.path))
 
     def add_frame(self, frame: Image, position: tuple = (0, 0)):
         self.framed = Image.new(frame.mode, frame.size)
         self.framed.paste(self.photo, position)
         self.framed.paste(frame, (0, 0), frame)
 
-    def resize(self, width: int):
-        ratio = self.photo.size[0] / self.photo.size[1]
+    @staticmethod
+    def resize_photo(photo: Image, width: int):
+        ratio = photo.size[0] / photo.size[1]
         height = int(width / ratio)
-        self.photo = self.photo.resize((width, height), Image.ANTIALIAS)
+        return photo.resize((width, height), Image.ANTIALIAS)
+
+    def resize(self, width: int):
+        self.photo = Photo.resize_photo(self.photo, width)
 
     def save(self):
         """Saving framed photo and duplicate it"""
-        name, extension = self.path[1].split('.')
-        copy_name = name + '%s.' + self.EXTENSION
+        if self.duplicate != 0:
+            name, extension = self.path[1].split('.')
+            copy_name = name + '%s.' + self.EXTENSION
 
-        if self.framed is None:
-            print("No framed photo found")
-            return
+            if self.framed is None:
+                print("No framed photo found")
+                return
 
-        for i in range(self.duplicate):
-            self.framed.save(
-                path.join(self.path[0],
-                          copy_name % ('_copy_' + str(i))),
-                self.EXTENSION)
+            for i in range(self.duplicate):
+                self.framed.save(
+                    path.join(self.path[0],
+                              copy_name % ('_copy_' + str(i))),
+                    self.EXTENSION)
 
         remove(path.join(self.path[0], self.path[1]))
 
     def light(self, ration):
         self.photo = self.photo.point(lambda p: p * ration)
+
+    @staticmethod
+    def bitmap_from_pil_image(caller, pil_image):
+        wx_image = wx.Image(pil_image.size[0], pil_image.size[1])
+        wx_image.SetData(pil_image.convert("RGB").tobytes())
+        bitmap = wx.Bitmap(wx_image)
+        return bitmap
